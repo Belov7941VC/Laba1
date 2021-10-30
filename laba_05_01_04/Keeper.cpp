e818aa77fc76f9b61d1ac870f34b25e810fb6d2a
Belov7941VC <goshabelov01@mail.ru>
2021-10-30 17:40:45 +0300

    реализовал методы в классе Keeper

--- laba_05_01_04/Keeper.cpp
+++ laba_05_01_04/Keeper.cpp
@@ -1,23 +1,78 @@
 #include "Keeper.h"
 
+Keeper::Keeper()
+{
+	std::cout << "Keeper\n";
+}
+
+Keeper::~Keeper()
+{
+	std::cout << "~Keeper\n";
+}
+
 void Keeper::print()
 {
+	for (size_t i = 0; i < animals.size(); i++)
+		std::cout << "Животное №" << i + 1 << ": " << animals[i]->get_to_print();
 }
 
 void Keeper::add(Animal* new_animal)
 {
+	animals.push_back(new_animal);
 }
 
 void Keeper::del(size_t num)
 {
+	delete animals.pop(num);
 }
 
-bool Keeper::save(std::string file)
+bool Keeper::save(string file)
 {
-	return false;
+	std::ofstream fout(file);
+	if (!fout) return false;
+	fout << to_string(animals.size()) << std::endl;
+	for (size_t i = 0; i < animals.size(); ++i)
+		fout << animals[i]->get_data();
+
+	fout.close();
+	return true;
 }
 
-bool Keeper::load(std::string file)
+bool Keeper::load(string file)
 {
-	return false;
+	std::ifstream fin(file);
+	if (!fin) return false;
+
+	std::string type, breed, color, temp1, temp2;
+	size_t animal_amount;
+
+	fin >> animal_amount;
+	for (size_t i = 0; i < animal_amount; i++)
+	{
+		fin >> type;
+		getline(fin, breed);
+		getline(fin, breed);
+		getline(fin, color);
+		getline(fin, temp1);
+		if (type == "Fish")
+		{
+			animals.push_back(new Fish(breed, color, temp1));
+		}
+		else if (type == "Bird")
+		{
+			getline(fin, temp2);
+			animals.push_back(new Bird(breed, color, temp1, temp2));
+		}
+		else if (type == "Cat")
+		{
+			getline(fin, temp2);
+			animals.push_back(new Cat(breed, color, temp1, temp2));
+		}
+	}
+	return true;
+}
+
+size_t Keeper::size()
+{
+	return animals.size();
 }
--- laba_05_01_04/Keeper.h
+++ laba_05_01_04/Keeper.h
@@ -1,17 +1,23 @@
 #pragma once
-#include "List.h"
-#include "Animal.h"
 #include <fstream>
 #include <string>
+#include "List.h"
+#include "Animal.h"
+#include "Fish.h"
+#include "Bird.h"
+#include "Cat.h"
 class Keeper
 {
 private:
-	List<Animal*> garages;
+	List<Animal*> animals;
 
 public:
+	Keeper();
+	~Keeper();
 	void print();					// вывод на экран производных объектов абстрактного класса Base
 	void add(Animal* new_animal);	// добавление     производных объектов абстрактного класса Base
 	void del(size_t num);			// удаление       производных объектов абстрактного класса Base
-	bool save(std::string file);			// полное сохранение     себя в  файле
-	bool load(std::string file);			// полное восстановление себя из файла
+	bool save(std::string file);	// полное сохранение     себя в  файле
+	bool load(std::string file);	// полное восстановление себя из файла
+	size_t size();
 };
